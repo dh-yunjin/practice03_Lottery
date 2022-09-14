0b5d8483eadc8ea6052b4ca2d373c05f84b25a86
r136a1x27 <r136a1x27@gmail.com>
2022-09-15 03:19:03 +0900

    1st submit

---
+++ src/Lottery.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Lottery{
+    uint256 balance;
+
+    constructor () {
+        
+    }
+
+
+    function buy(uint ) public payable{ // 반환값이 따로 없으면 returns 안적어줘도 됨
+    // {value: 값} 이런 식으로 넘겨주면 payable을 작성해줘야 하는 듯
+
+    }
+    function draw() public returns(자료형){
+
+    }
+    function claim() public returns(자료형){
+        
+    }
+    function getNextWinningNumber() public view returns(uint16 memory){
+
+    }
+}
