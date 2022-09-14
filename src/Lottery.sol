dcbe5465b28add8a15be17fe698177c35ece39f9
r136a1x27 <r136a1x27@gmail.com>
2022-09-15 03:22:26 +0900

    2st submit

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -2,24 +2,115 @@
 pragma solidity ^0.8.13;
 
 contract Lottery{
-    uint256 balance;
-
+    mapping(address => uint256) public lottery_balances; // 예치금(구매한 lottery)
+    uint16 _winningNumber;
+    mapping(address => uint16) public _bettingNumber;
+    uint256 balance; // testClaimOnWin 에서...lottery도 자체적인 balance를 갖고있어야 함
+    // 그냥 직관적으로 이해해보면 lottery를 구매할 때 마다 balance를 늘려주면 되지 않을까 싶음
+    // 로또 시스템 자체가 구매금 모두 모아서 1명에게 몰빵 개념이다 보니까
     constructor () {
+        _winningNumber = 0;
+    }
+
+/* 해결 순서
+    testInsufficientFunds1~3
+    testGoodBuy
+    testNoDuplicate
+    testNoBuyAfterPhaseEnd (동시) testSellPhaseFullLength
+
+    testNoDrawDuringSellPhase
+    getNextWinningNumber
+
+    testNoClaimOnLose (동시) testNoClaimOnWin // 제대로 해결 X
+    testNoDrawDuringClaimPhase
+
+    testRollover // Claim이 해결되어야 함
+
+    testSplit // 
+*/
+    function buy(uint16 bettingNumber) public payable{ // 반환값이 따로 없으면 returns 안적어줘도 됨
+        // uint16이 맞는지..._winningNumber가 어떤 역할인지...죄다 buy(0)인데 winningNumber만 변수임
+
+        // {value: 값} 이런 식으로 넘겨주면 payable을 작성해줘야 하는 듯
+        // 사용은 msg.value
+        // https://dayone.tistory.com/33
+        // payable을 통해서 이더를 받는 함수임을...
+
+        // msg.sender와 user의 차이 - msg.sender는 contract에 해당하지 않을까...
+        // 구분이 필요 - 근데 Lottery.sol에서도 address(1) 이런식으로 접근이 가능한가?
+        // (착각) parameter가 user인줄 알았는데 bettingNumber였음
+
+        require(lottery_balances[msg.sender] == 0);
+        // testNoDuplicate 이미 구입한 상태에서 다시 구입 불가
+
+        require(block.timestamp <= 24 hours);
+        // testNoBuyAfterPhaseEnd 
+        // testSellPhaseFullLength
+        // 이렇게 간단하다고?
+        // block은 컨트랙트에 귀속되는건가?        
+
+        require(msg.value != 0);
+        // testInsufficientFunds1 // 복권을 0 eth 이상은 구매해야
+
+        require(msg.sender.balance >= msg.value); // 복권을 구매할 잔고가 충분한지
+        // testInsufficientFunds2
+
+        require(msg.value == 0.1 ether); // 최대(고정) 베팅 금액?
+        // testInsufficientFunds3
+        
+        // msg.sender.balance -= msg.value; // 잔고 차감은 컨트랙트 들어오면서 지불된듯?
+        lottery_balances[msg.sender] += msg.value; // 구입 금액
+        _bettingNumber[msg.sender] = bettingNumber;// claim
+        balance += msg.value; //claim
+    }
+
+    function draw() public{ 
+        // 그래서 대체 무슨 역할인지... - 단어 상으로는 "인출"
+        // ★★★ 이 draw 단계에서 뭐가 부족해서 claim도 안되는 것이 아닐까 유추해봄
+        require(block.timestamp > 24 hours);
+        // testNoDrawDuringSellPhase
+
+        require(lottery_balances[msg.sender] != 0);
+        // testNoDrawDuringClaimPhase
+        // claim 중에 draw를 수행하지 못하게 → claim 수행 시 반드시 lottery_balnaces 초기화 과정을 거치므로...
+    } 
+
+    function claim() public{ // quiz에서 했다시피..."지불"하는 것을 의미함(결과에 따라)
+        require(block.timestamp > 24 hours);
+        // testNoClaimDuringSellPhase (근데 이거 적기 전에도 pass였음)
+        
+        // msg.value = msg.sender.balance; 으로 대입 불가 (둘 다 left value임)
+        if(_winningNumber == _bettingNumber[msg.sender]){
+            // payable(msg.sender).transfer(balance); // 누가 어떤 자금으로 송금하는것인지?
+            // ★★★개념은 맞는 것 같은데 뭔가 송금이 안되는데, 이유를 모르겠습니다★★★
+            // testClaimOnWin testNoClaimOnLose
+            lottery_balances[msg.sender] = 0;
+            balance = 0;
+            
+
+        }
+        // WIN, 지금까지 쌓인 금액 = lottery.balance를 가지게 됨
+        else{
+            lottery_balances[msg.sender] = 0;
+        }
+
+        // assertEq(received_msg_value, expectedPayout); 를 만족시켜야 함
+        // received_msg_value는 receive()에 의해 msg.value와 동일함
+        // expectedPayout은 address(lottery).balance; - 변수 선언 필요성
+
+
+        /* 참고용: practice02_Quiz
+        uint256 amount = lottery_balances[msg.sender];
+        lottery_balances[msg.sender] = 0; // 베팅한 금액 초기화
+        payable(msg.sender).transfer(amount); // 상금 주기
+        // win, lose에 따라 amount가 변동됨 */
         
     }
+    function winningNumber() public view returns(uint16){
 
 
-    function buy(uint ) public payable{ // 반환값이 따로 없으면 returns 안적어줘도 됨
-    // {value: 값} 이런 식으로 넘겨주면 payable을 작성해줘야 하는 듯
 
-    }
-    function draw() public returns(자료형){
-
-    }
-    function claim() public returns(자료형){
-        
-    }
-    function getNextWinningNumber() public view returns(uint16 memory){
-
+        return _winningNumber;
+        // getNextWinningNumber
     }
 }
--- test/Lottery.t.sol
+++ test/Lottery.t.sol
@@ -10,6 +10,7 @@ contract LotteryTest is Test {
     function setUp() public {
        lottery = new Lottery();
        received_msg_value = 0;
+       // vm 객체는 foundry에서...https://jamesbachini.com/foundry-tutorial/
        vm.deal(address(this), 100 ether);
        vm.deal(address(1), 100 ether);
        vm.deal(address(2), 100 ether);
@@ -33,63 +34,99 @@ contract LotteryTest is Test {
     function testInsufficientFunds3() public {
         vm.expectRevert();
         lottery.buy{value: 0.1 ether + 1}(0);
+        // 이건 왜 실패가 떠야만 하는지
+        // 최대 베팅 금액을 0.1 ether로 고정하기 위함인가?
+        // -vvv로 값을 보면 0.1 ether는 100000000000000000이고, 1을 빼면 9999..999
     }
 
     function testNoDuplicate() public {
         lottery.buy{value: 0.1 ether}(0);
+
         vm.expectRevert();
         lottery.buy{value: 0.1 ether}(0);
-    }
+    } // 한 번에 1개의 lotto만 구매할 수 있도록...
 
-    function testSellPhaseFullLength() public {
+    function testSellPhaseFullLength() public { // 함수명이 뭘 의미하는 것인지
         lottery.buy{value: 0.1 ether}(0);
-        vm.warp(block.timestamp + 24 hours - 1);
-        vm.prank(address(1));
-        lottery.buy{value: 0.1 ether}(0);
+
+        vm.warp(block.timestamp + 24 hours - 1); // 블록의 타임스탬프 설정
+        // 왜 이런 값으로 설정하는 것인지? 왜 1을 빼는지?
+        // 유추: 24시간이 지나면 전환 후 구매 불가
+        // (즉 24시가 최대이기때문에 Phase FullLength)
+        // block.timestamp = 생성되자마자 1로 세팅됨, 24 hours를 더하면 86401
+        // 여기서 1을 빼니까 86400이 됨
+
+        // 왜 구매가 불가능한 것인지...this가 발행자의 입장도 아닌데
+
+        vm.prank(address(1)); // setting the next call's msg.sender as address
+        // 정확히 어떤 것을 의미하는것인지 - this에서 1로 전환?
+
+        lottery.buy{value: 0.1 ether}(0); // 전환 후 구매 - 정상
     }
 
     function testNoBuyAfterPhaseEnd() public {
         lottery.buy{value: 0.1 ether}(0);
-        vm.warp(block.timestamp + 24 hours);
+        
+        vm.warp(block.timestamp + 24 hours); 
+        // 바로 위인 testSellPhaseFullLength와 비교했을 때, 
+        // 달라진 점은 24 hours에 1을 빼지 않는 것 뿐...(86401)
+        // 유추: 딱 24시간이 지나면 전환 후 구매가 불가
+
         vm.expectRevert();
-        vm.prank(address(1));
+        vm.prank(address(1)); 
+
         lottery.buy{value: 0.1 ether}(0);
     }
+    // ---사실 상 여기까지 buy의 조건에 해당함
+    // 0.1 ether + 1이 동작하지 않도록 할 것, 복제되지 않도록 할 것, 거래 끝난 다음 다른 buy 하지 않도록 할 것...
 
-    function testNoDrawDuringSellPhase() public {
-        lottery.buy{value: 0.1 ether}(0);
-        vm.warp(block.timestamp + 24 hours - 1);
-        vm.expectRevert();
-        lottery.draw();
-    }
-
-    function testNoClaimDuringSellPhase() public {
-        lottery.buy{value: 0.1 ether}(0);
-        vm.warp(block.timestamp + 24 hours - 1);
-        vm.expectRevert();
-        lottery.claim();
-    }
-
+    // ---아래부터 draw, claim의 조건
     function testDraw() public {
         lottery.buy{value: 0.1 ether}(0);
         vm.warp(block.timestamp + 24 hours);
         lottery.draw();
     }
 
+    function testNoDrawDuringSellPhase() public {
+        lottery.buy{value: 0.1 ether}(0);
+        vm.warp(block.timestamp + 24 hours - 1); 
+
+        vm.expectRevert();
+        lottery.draw();
+    } // testDraw와 비교했을 때 warp만 달라졌음 - 위와 동일하게 require 시간 조건
+    // 다른 점은 반대로 24시간 전에는 draw를 못한다는 점..
+    // -----------------------------
+
+    function testNoClaimDuringSellPhase() public {
+        lottery.buy{value: 0.1 ether}(0);
+        vm.warp(block.timestamp + 24 hours - 1);
+
+        vm.expectRevert();
+        lottery.claim();
+    }
+    // 위와 동일하게 require 시간 조건
+
     function getNextWinningNumber() private returns (uint16) {
         uint256 snapshotId = vm.snapshot();
+
         lottery.buy{value: 0.1 ether}(0);
+
         vm.warp(block.timestamp + 24 hours);
+
         lottery.draw();
+
         uint16 winningNumber = lottery.winningNumber();
+
         vm.revertTo(snapshotId);
+
         return winningNumber;
-    }
+    } // 단순하게 winningNumber return만 해주면 됨
+    // nextWinningNumber를 조작하는 로직이 draw에서 있을 것 같은데...
 
     function testClaimOnWin() public {
         uint16 winningNumber = getNextWinningNumber();
         lottery.buy{value: 0.1 ether}(winningNumber); vm.warp(block.timestamp + 24 hours);
-        uint256 expectedPayout = address(lottery).balance;
+        uint256 expectedPayout = address(lottery).balance; // lottery의 balance 개념
         lottery.draw();
         lottery.claim();
         assertEq(received_msg_value, expectedPayout);
@@ -102,43 +139,56 @@ contract LotteryTest is Test {
         lottery.claim();
         assertEq(received_msg_value, 0);
     }
+    // Win이랑 Lose의 차이점이 buy에서 전달되는 winningNumber인걸 보니...buy 시 bettingNumber를 전달해주는 개념같음
 
     function testNoDrawDuringClaimPhase() public {
         uint16 winningNumber = getNextWinningNumber();
         lottery.buy{value: 0.1 ether}(winningNumber); vm.warp(block.timestamp + 24 hours);
         lottery.draw();
         lottery.claim();
+
         vm.expectRevert();
         lottery.draw();
     }
-
+    // -------------------------------------
     function testRollover() public {
         uint16 winningNumber = getNextWinningNumber();
         lottery.buy{value: 0.1 ether}(winningNumber + 1); vm.warp(block.timestamp + 24 hours);
         lottery.draw();
         lottery.claim();
+        // 져서 0.1 ether가 lottery에 저장되는게 맞음
 
         winningNumber = getNextWinningNumber();
         lottery.buy{value: 0.1 ether}(winningNumber); vm.warp(block.timestamp + 24 hours);
         lottery.draw();
         lottery.claim();
+        // 이겨서 앞에서 저장된 0.1 ether랑 이번에 베팅한 0.1 ether를 가져옴
+
         assertEq(received_msg_value, 0.2 ether);
-    }
+    } // 여기서 nextWinningNumber 로직이 밝혀질 줄 알았는데 그냥 winningNumber는 0 고정인...
 
     function testSplit() public {
         uint16 winningNumber = getNextWinningNumber();
         lottery.buy{value: 0.1 ether}(winningNumber);
+
+        // 주소 전환
         vm.prank(address(1));
         lottery.buy{value: 0.1 ether}(winningNumber);
+
+        // 잔고 설정 - 초깃값 100eth - 0.1ether에서 0으로...
         vm.deal(address(1), 0);
+
         vm.warp(block.timestamp + 24 hours);
         lottery.draw();
-
         lottery.claim();
+
         assertEq(received_msg_value, 0.1 ether);
 
+        // 주소 전환 (위에서 했는데 동일한 주소로 왜 또?)
+        // Sets the next calls니까... 모르겠다
         vm.prank(address(1));
         lottery.claim();
+
         assertEq(address(1).balance, 0.1 ether);
     }
 
